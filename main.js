48f60b0627419afd073ef2d1f698a38db519662f
Velocifero1052 <commelevent1052@yandex.ru>
2023-04-09 19:07:24 +0100

    adding days and iteration functionalities

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-let start = '2023-04-09 14:40';
+let start = '2023-03-09 14:40';
 let end = '2023-04-09 16:40';
 
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -22,6 +22,16 @@ let secondDate = new Date(+secondYearMonthDay[0], +secondYearMonthDay[1], +secon
 
 
 console.log("Start: " + weekday[firstDate.getDay()] + " at " + firstDate.getHours() + ":" + firstDate.getMinutes());
+
+//adding days
+console.log(firstDate);
+firstDate.setDate(firstDate.getDate() + 1);
+console.log(firstDate);
+
+for(let currentDay = firstDate.getTime(); currentDay < secondDate.getTime(); currentDay += 86_400_000){
+    console.log(currentDay);
+}
+
 console.log("End: " + weekday[secondDate.getDay()] + " at " + secondDate.getHours() + ":" + secondDate.getMinutes())
 
 
